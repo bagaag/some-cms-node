fedac625f633c21629957634f7723c054a4cb113
OpenShift guest <wiseley@gmail.com>
2012-12-25 21:03:39 -0500

    Refactor for controllers, routes

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
+// database connection
+var config = require('./config.js');
+var mongoose = require('mongoose');
+mongoose.connect(config.db.mongodb);
+
 // module dependencies
-var express = require('express')
-  , cons = require('consolidate')
-  , config = require('./config.js')
-  , mongoose = require('mongoose')
-  , models = require('./models')({ mongoose: mongoose })
-  , app = module.exports = express();
+var express = require('express');
+var routes = require('./routes.js');
+var models = require('./models')({mongoose: mongoose});
+var cons = require('consolidate');
+var controllers = require('./controllers')({mongoose: mongoose});
+var app = module.exports = express();
 
 // configure templates & views
 app.engine('html', cons.mustache);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-// connect db
-mongoose.connect(config.db.mongodb);
+// middleware 
+//app.use(express.bodyParser()); // parse form posts
+//app.use(express.methodOverride()); // allows put/get/post overrides
+//app.use(express.router); // enables app.get, app.post, etc.
+app.use(express.static(__dirname + '/public'));
 
-// test mustache
-app.get('/', function(req, res){
-  var viewdata = { 'test' : 'Hey now.'};
-  res.render('index', viewdata);
-});
-
-// test mongohq
-app.get('/db', function(req, res){
-  var Pages = mongoose.model('Pages');
-  Pages.find({}, function(err, pages) {
-    var viewdata = { 'list' : pages};
-    res.render('index', viewdata);
-  });
-});
+// routes
+routes.setup({
+  'controllers': controllers,
+  'app': app
+ });
 
 // configure error handling
 if (app.get('env')=='development') {
